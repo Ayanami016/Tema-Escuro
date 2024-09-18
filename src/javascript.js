f06465fe4eaacb17d6b84b0e31c6aab6a0189bff
Isa <kmasi4@gmail.com>
2024-09-18 19:52:29 -0300

    botoes

--- src/javascript.js
+++ src/javascript.js
@@ -1,12 +1,14 @@
 var favicon = document.getElementById("favicon")
+var tituloaba = document.getElementById("titulo")
 var bg = document.getElementById("conteudo")
 var txtclaro = document.querySelector(".textoclaro")
 var txtmeio = document.querySelector(".textomeio")
 var txtescuro = document.querySelector(".textoescuro")
 
-function tema(fundo, fav, txtmostrar, txtesconder) {
+function tema(fundo, fav, titulo, txtmostrar, txtesconder) {
     bg.style.backgroundColor = fundo
     favicon.href = fav
+    document.title = titulo
     txtmostrar.style.display = "inherit"
     txtesconder.forEach(function(texto) {
         texto.style.display = "none";
@@ -15,15 +17,15 @@ function tema(fundo, fav, txtmostrar, txtesconder) {
 
 // Tema Claro
 document.getElementById("claro").addEventListener('click', function() {
-    tema("#fff", "src/img/fav-sol.ico", txtclaro, [txtmeio, txtescuro])
+    tema("#fff", "src/img/fav-sol.ico", "Tema Claro", txtclaro, [txtmeio, txtescuro])
 })
 
 // Tema Meio Escuro
 document.getElementById("meio-escuro").addEventListener('click', function() {
-    tema("#364478", "src/img/fav-meialua.ico", txtmeio, [txtclaro, txtescuro])
+    tema("#364478", "src/img/fav-meialua.ico", "Tema Meio Escuro", txtmeio, [txtclaro, txtescuro])
 })
 
 // Tema Escuro
 document.getElementById("escuro").addEventListener('click', function() {
-    tema("#1b1d38", "src/img/fav-lua.ico", txtescuro, [txtclaro, txtmeio])
+    tema("#1b1d38", "src/img/fav-lua.ico", "Tema Escuro", txtescuro, [txtclaro, txtmeio])
 })
