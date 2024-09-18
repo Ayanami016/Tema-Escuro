f993a9b547ea21c5e38a2368075cfad5eacc15f1
Isa <kmasi4@gmail.com>
2024-09-18 19:39:47 -0300

    otimização  js

--- src/javascript.js
+++ src/javascript.js
@@ -4,29 +4,26 @@ var txtclaro = document.querySelector(".textoclaro")
 var txtmeio = document.querySelector(".textomeio")
 var txtescuro = document.querySelector(".textoescuro")
 
+function tema(fundo, fav, txtmostrar, txtesconder) {
+    bg.style.backgroundColor = fundo
+    favicon.href = fav
+    txtmostrar.style.display = "inherit"
+    txtesconder.forEach(function(texto) {
+        texto.style.display = "none";
+    });
+}
+
 // Tema Claro
-var claro = document.getElementById("claro").addEventListener('click', function() {
-    bg.style.backgroundColor =  "#fff"
-    favicon.href = "src/img/fav-sol.ico"
-    txtclaro.style = "display: inherit"
-    txtescuro.style = "display: none"
-    txtmeio.style = "display: none"
+document.getElementById("claro").addEventListener('click', function() {
+    tema("#fff", "src/img/fav-sol.ico", txtclaro, [txtmeio, txtescuro])
 })
 
 // Tema Meio Escuro
-var meio = document.getElementById("meio-escuro").addEventListener('click', function() {
-    bg.style.backgroundColor = "#364478"
-    favicon.href = "src/img/fav-meialua.ico"
-    txtmeio.style = "display: inherit; color: #fff"
-    txtclaro.style = "display: none"
-    txtescuro.style = "display: none"
+document.getElementById("meio-escuro").addEventListener('click', function() {
+    tema("#364478", "src/img/fav-meialua.ico", txtmeio, [txtclaro, txtescuro])
 })
 
 // Tema Escuro
-var escuro = document.getElementById("escuro").addEventListener('click', function() {
-    bg.style.backgroundColor = "#1b1d38"
-    favicon.href = "src/img/fav-lua.ico"
-    txtescuro.style = "display: inherit; color: #fff"
-    txtclaro.style = "display: none"
-    txtmeio.style = "display: none"
+document.getElementById("escuro").addEventListener('click', function() {
+    tema("#1b1d38", "src/img/fav-lua.ico", txtescuro, [txtclaro, txtmeio])
 })
